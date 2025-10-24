8f61cb5306cdbd33fc88019ca29acb8acee04ec3
poojaramalingam <poojaramalingam@gmail.com>
2025-10-24 17:01:26 +0100

    Delete Band_Name_Generator.py

--- Band_Name_Generator.py
+++
@@ -1,6 +0,0 @@
-#Day 1 of Coding
-#24 Oct 2025
-print("Welcome to the band name generator")
-city=input("What is the name of the city you grew up in? \n")
-pet=input("what's your pet's name?\n")
-print("Your band name is " + city + " " + pet )
